1294012bd3bd59ec5e31491792f09cafe027df0a
linkcoding <kenny.lin.91@gmail.com>
2017-08-06 14:06:09 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    #binding.pry
+    self.joins(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    self.joins(boats: :classifications).group(:captain_id).where(classifications: {name: "Sailboat"})
+  end
+
+  def self.talented_seamen
+    m = self.joins(boats: :classifications).group(:captain_id).where(classifications: {name: "Motorboat"})
+    self.where(id: (self.sailors & m))
+  end
+
+  def self.non_sailors
+    ids = self.sailors.select(:id)
+    self.where.not(id: ids)
+    #self.all - self.sailors
+    #this will only return an array
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,14 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    #tentative : self.joins(:boats).order("boats.length DESC").limit(2)
+    max_length = self.joins(:boats).maximum('boats.length')
+    self.joins(:boats).where(boats: {length: max_length})
+  end
 end
